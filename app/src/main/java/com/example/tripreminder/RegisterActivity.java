4199c288f9615e55aad216eeea6fe3fe8625805e
Rahma Ayman <rahma.ayman2016@gmail.com>
2021-02-23 19:25:48 +0200

    add app icon

--- app/src/main/java/com/example/tripreminder/RegisterActivity.java
+++ app/src/main/java/com/example/tripreminder/RegisterActivity.java
@@ -141,6 +141,10 @@ public class RegisterActivity extends AppCompatActivity {
             Toast.makeText(this,getString(R.string.please_enter_password), Toast.LENGTH_SHORT).show();
             valid=false;
         }
+        if (password.length()<6){
+            Toast.makeText(this,getString(R.string.password_validation), Toast.LENGTH_SHORT).show();
+            valid=false;
+        }
         return valid;
     }
 
